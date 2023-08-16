9e2894dffb91e92913139fa3d444bfb8eef484f4
Eduardo González Sánchez <48300381+geduardo@users.noreply.github.com>
2023-08-16 17:56:27 +0200

    Update setup.py

--- edm_environments/setup.py
+++ edm_environments/setup.py
@@ -4,5 +4,5 @@ setup(
     name="edm_environments",
     version="0.0.1",
     packages=find_packages(include=['envs*', 'wrappers*']),
-    install_requires=["gymnasium==0.29.0", "pygame==2.1.0"],
+    install_requires=["gymnasium==0.29.0", "pygame==2.5"],
 )
